e5ec0ca8bbbb6952bf6fb5b6d259974f5ca7538f
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-12-05 10:32:36 +0100

    Take input from command line args

--- 2019/p05/p05b.zig
+++ 2019/p05/p05b.zig
@@ -8,13 +8,19 @@ pub fn main() !void {
     const allocator = std.debug.global_allocator;
     var buf = try std.Buffer.initSize(allocator, 0);
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
     var count: u32 = 0;
     while (std.io.readLine(&buf)) |line| {
         count += 1;
         var computer = Computer.init(line);
-        const input: i32 = 5;
-        const result: i32 = computer.run(input);
-        try out.print("Result for {} is {}\n", input, result);
+        for (args) |arg, pos| {
+            if (pos == 0) continue;
+            const input = try std.fmt.parseInt(i32, arg, 10);
+            const result = computer.run(input);
+            try out.print("Result for {} is {}\n", input, result);
+        }
     } else |err| {
         // try out.print("Error, {}!\n", err);
     }
