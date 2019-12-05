0d314b15ed21708427161e251359a224b3aac50b
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-12-05 11:32:41 +0100

    Add unit tests for day 2 \o/

--- 2019/p02/computer.zig
+++ 2019/p02/computer.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
 pub const Computer = struct {
     mem: [1024]u32,
@@ -10,7 +11,7 @@ pub const Computer = struct {
         STOP = 99,
     };
 
-    pub fn init(str: []u8) Computer {
+    pub fn init(str: []const u8) Computer {
         var self = Computer{
             .mem = undefined,
             .pos = 0,
@@ -63,3 +64,38 @@ pub const Computer = struct {
         }
     }
 };
+
+test "simple - pos 0 becomes 3500" {
+    const data: []const u8 = "1,9,10,3,2,3,11,0,99,30,40,50";
+    var computer = Computer.init(data[0..]);
+    computer.run();
+    assert(computer.get(0) == 3500);
+}
+
+test "simple - pos 0 becomes 2" {
+    const data: []const u8 = "1,0,0,0,99";
+    var computer = Computer.init(data[0..]);
+    computer.run();
+    assert(computer.get(0) == 2);
+}
+
+test "simple - pos 3 becomes 6" {
+    const data: []const u8 = "2,3,0,3,99";
+    var computer = Computer.init(data[0..]);
+    computer.run();
+    assert(computer.get(3) == 6);
+}
+
+test "simple - pos 5 becomes 9801" {
+    const data: []const u8 = "2,4,4,5,99,0";
+    var computer = Computer.init(data[0..]);
+    computer.run();
+    assert(computer.get(5) == 9801);
+}
+
+test "simple - pos 0 becomes 30" {
+    const data: []const u8 = "1,1,1,4,99,5,6,0,99";
+    var computer = Computer.init(data[0..]);
+    computer.run();
+    assert(computer.get(0) == 30);
+}
