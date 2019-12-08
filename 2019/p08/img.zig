bae46e82d90848803a3334b9de5ce77ab76f8709
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-12-08 12:24:32 +0100

    Better, clearer names

--- 2019/p08/img.zig
+++ 2019/p08/img.zig
@@ -24,15 +24,15 @@ pub const Image = struct {
         // self.allocator.free(self.data);
     }
 
-    fn pos(self: Image, l: usize, h: usize, w: usize) usize {
-        return l * self.w * self.h + h * self.w + w;
+    fn pos(self: Image, l: usize, c: usize, r: usize) usize {
+        return (l * self.h + r) * self.w + c;
     }
 
     pub fn parse(self: *Image, data: []const u8) void {
         var j: usize = 0;
-        var pl: usize = 0;
-        var ph: usize = 0;
-        var pw: usize = 0;
+        var l: usize = 0;
+        var r: usize = 0;
+        var c: usize = 0;
 
         const layer_size = self.w * self.h;
         const num_layers = (data.len + layer_size - 1) / layer_size;
@@ -47,37 +47,37 @@ pub const Image = struct {
             std.debug.warn("ALLOC {} layers\n", self.l);
         }
         while (j < data.len) : (j += 1) {
-            const c = data[j] - '0';
-            self.data[self.pos(pl, ph, pw)] = c;
-            // std.debug.warn("DATA {} {} {} = {}\n", pl, ph, pw, c);
-            pw += 1;
-            if (pw >= self.w) {
-                pw = 0;
-                ph += 1;
+            const color = data[j] - '0';
+            self.data[self.pos(l, c, r)] = color;
+            // std.debug.warn("DATA {} {} {} = {}\n", l, r, c, color);
+            c += 1;
+            if (c >= self.w) {
+                c = 0;
+                r += 1;
             }
-            if (ph >= self.h) {
-                pw = 0;
-                ph = 0;
-                pl += 1;
+            if (r >= self.h) {
+                c = 0;
+                r = 0;
+                l += 1;
             }
         }
-        self.l = pl;
+        self.l = l;
     }
 
     pub fn find_layer_with_fewest_zeros(self: *Image) usize {
         var m0: usize = std.math.maxInt(u32);
         var ml: usize = 0;
         var mp: usize = 0;
-        var pl: usize = 0;
-        while (pl < self.l) : (pl += 1) {
+        var l: usize = 0;
+        while (l < self.l) : (l += 1) {
             var c0: usize = 0;
             var c1: usize = 0;
             var c2: usize = 0;
-            var ph: usize = 0;
-            while (ph < self.h) : (ph += 1) {
-                var pw: usize = 0;
-                while (pw < self.w) : (pw += 1) {
-                    switch (self.data[self.pos(pl, ph, pw)]) {
+            var r: usize = 0;
+            while (r < self.h) : (r += 1) {
+                var c: usize = 0;
+                while (c < self.w) : (c += 1) {
+                    switch (self.data[self.pos(l, c, r)]) {
                         0 => c0 += 1,
                         1 => c1 += 1,
                         2 => c2 += 1,
@@ -85,10 +85,10 @@ pub const Image = struct {
                     }
                 }
             }
-            // std.debug.warn("LAYER {} has {} zeros\n", pl, c0);
+            // std.debug.warn("LAYER {} has {} zeros\n", l, c0);
             if (m0 > c0) {
                 m0 = c0;
-                ml = pl;
+                ml = l;
                 mp = c1 * c2;
             }
         }
@@ -97,18 +97,18 @@ pub const Image = struct {
     }
 
     pub fn render(self: *Image) void {
-        var ph: usize = 0;
-        while (ph < self.h) : (ph += 1) {
-            var pw: usize = 0;
-            while (pw < self.w) : (pw += 1) {
-                var pl: usize = 0;
-                while (pl < self.l) : (pl += 1) {
-                    const c = self.data[self.pos(pl, ph, pw)];
-                    if (c == 2) {
+        var r: usize = 0;
+        while (r < self.h) : (r += 1) {
+            var c: usize = 0;
+            while (c < self.w) : (c += 1) {
+                var l: usize = 0;
+                while (l < self.l) : (l += 1) {
+                    const color = self.data[self.pos(l, c, r)];
+                    if (color == 2) {
                         continue;
-                    } else if (c == 0) {
+                    } else if (color == 0) {
                         std.debug.warn(" ");
-                    } else if (c == 1) {
+                    } else if (color == 1) {
                         std.debug.warn("\u{2588}");
                     } else {}
                     break;
