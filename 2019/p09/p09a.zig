4aa4c84b26e1bb8bc566b337c267d75f23d0024a
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-12-09 13:38:26 +0100

    Day 9

---
+++ 2019/p09/p09a.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const Computer = @import("./computer.zig").Computer;
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut() catch unreachable;
+    const out = &stdout.outStream().stream;
+
+    const allocator = std.debug.global_allocator;
+    var buf = try std.Buffer.initSize(allocator, 0);
+
+    var count: u32 = 0;
+    while (std.io.readLine(&buf)) |line| {
+        count += 1;
+        var computer = Computer.init(line);
+        defer computer.deinit();
+        computer.setReentrant();
+
+        computer.enqueueInput(1);
+        while (true) {
+            computer.run();
+            if (computer.halted) break;
+        }
+        var j: usize = 0;
+        while (j < computer.outputs.pw) : (j += 1) {
+            std.debug.warn("{}\n", computer.outputs.data[j]);
+        }
+        std.debug.warn("DONE\n");
+    } else |err| {
+        // try out.print("Error, {}!\n", err);
+    }
+    try out.print("Read {} lines\n", count);
+}
