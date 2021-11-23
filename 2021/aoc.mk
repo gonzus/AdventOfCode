e2c6dc94f39c4bff25cab4bbe937490d537fc200
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-11-23 16:31:57 +0100

    Ready for 2021!

---
+++ 2021/aoc.mk
@@ -0,0 +1,44 @@
+DATA = ../data/input$(DAY).txt
+PROGRAMS = p$(DAY)a p$(DAY)b
+
+define make-aoc-mod-target
+  $1_test:
+	zig test $1
+
+  test:: $1_test
+endef
+
+define make-aoc-prg-target
+  $1: $1.zig $(MODULES)
+	zig build-exe $1.zig
+  $1_clean:
+	rm -f $1
+  $1_run: $1
+	./$1 < $(DATA)
+
+  all:: $1
+  clean:: $1_clean
+  run:: $1_run
+endef
+
+$(foreach prg,$(PROGRAMS),$(eval $(call make-aoc-prg-target,$(prg))))
+$(foreach mod,$(MODULES) ,$(eval $(call make-aoc-mod-target,$(mod))))
+
+.PHONY: clean
+clean:: ## clean all
+	rm -fr zig-cache *.o *.swp
+
+.PHONY: all
+all:: ## build all parts of this problem
+
+.PHONY: run
+run:: ## run all parts of this problem
+
+.PHONY: test
+test:: ## test all modules
+
+# Borrowed from https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+.PHONY: help
+help: ## display this help section
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z0-9_-]+:.*?## / {printf "\033[36m%-38s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+.DEFAULT_GOAL := all
