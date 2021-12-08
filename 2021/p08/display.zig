0a23c82747b24eec2f46e10099680d2dfab803ba
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-12-08 16:29:27 +0100

    2021 day 08: change code into configuration
    For this we define a couple of arrays where we can store specific
    configs that can help with the algorithm (and make it shorter).

--- 2021/p08/display.zig
+++ 2021/p08/display.zig
@@ -3,9 +3,6 @@ const testing = std.testing;
 const allocator = std.testing.allocator;
 
 pub const Display = struct {
-    const MAX_DIGIT = 10;
-    const MAX_SEGMENT = 7;
-
     // digit  segments  length  digits with same length
     // -----  --------  ------  -----------------------
     //     0  abc.efg   6       0, 6, 9
@@ -19,16 +16,70 @@ pub const Display = struct {
     //     7  a.c..f.   3       7
     //     8  abcdefg   7       8
 
-    count_unique: [MAX_DIGIT]usize,
-    mask: [MAX_SEGMENT]u8,
+    const NUM_DIGITS = 10;
+    const NUM_SEGMENTS = 7;
+
+    // Given the length of a set of segments, determine which unique digit
+    // could be mapped by that segment length.  A zero value indicates either
+    // no digits or more than one digit, so digits are non-unique for that
+    // length.
+    //
+    // Example: a set of 3 segments can only denote a 7 digit.
+    const digits_for_len = [NUM_SEGMENTS + 1]u8{
+        0, // 0
+        0, // 1
+        1, // 2
+        7, // 3
+        4, // 4
+        0, // 5
+        0, // 6
+        8, // 7
+    };
+
+    // Given the length of a set of segments, determine which segments are
+    // definitely part of all the digits that could be mapped to that segment
+    // length.
+    //
+    // Example: a set of 5 segments could correspond to a 2, 3 or 4 digit; the
+    // segments that are shared by all of those digits are 'a', 'd' and 'g'.
+    const segments_for_len = [NUM_SEGMENTS + 1][]const u8{
+        "", // 0
+        "", // 1
+        "cf", // 2
+        "acf", // 3
+        "bcdf", // 4
+        "adg", // 5
+        "abfg", // 6
+        "abcdefg", // 7
+    };
+
+    // For each digit, the corresponding mask has a 1 if that digit uses that
+    // given segment in the display.
+    // Example: digit 7 uses segments 'a', 'c' & 'f'.
+    const digit_mask = [NUM_DIGITS]u8{
+        0b1110111, // 0
+        0b0010010, // 1
+        0b1011101, // 2
+        0b1011011, // 3
+        0b0111010, // 4
+        0b1101011, // 5
+        0b1101111, // 6
+        0b1010010, // 7
+        0b1111111, // 8
+        0b1111011, // 9
+    };
+
+    count_unique: [NUM_DIGITS]usize,
+    mask: [NUM_SEGMENTS]u8,
     total_sum: usize,
 
     pub fn init() Display {
         var self = Display{
-            .count_unique = [_]usize{0} ** MAX_DIGIT,
-            .mask = [_]u8{0} ** MAX_SEGMENT,
+            .count_unique = [_]usize{0} ** NUM_DIGITS,
+            .mask = [_]u8{0} ** NUM_SEGMENTS,
             .total_sum = 0,
         };
+
         return self;
     }
 
@@ -51,11 +102,11 @@ pub const Display = struct {
             if (pos == 1) {
                 self.propagate_masks();
 
-                var mapping: [MAX_SEGMENT]u8 = [_]u8{0} ** MAX_SEGMENT;
+                var mapping: [NUM_SEGMENTS]u8 = [_]u8{0} ** NUM_SEGMENTS;
                 for (self.mask) |m, j| {
                     // std.debug.warn("MASK {}: {b:0>7}\n", .{ j, m });
                     const c = @ctz(usize, m);
-                    mapping[MAX_SEGMENT - 1 - c] = @as(u8, 1) << @intCast(u3, MAX_SEGMENT - 1 - j);
+                    mapping[NUM_SEGMENTS - 1 - c] = @as(u8, 1) << @intCast(u3, NUM_SEGMENTS - 1 - j);
                 }
 
                 var itd = std.mem.tokenize(u8, str, " ");
@@ -64,12 +115,13 @@ pub const Display = struct {
                     self.update_unique_counts(segments);
 
                     var mask: u8 = 0;
-                    for (segments) |s| {
-                        mask |= mapping[s - 'a'];
+                    for (segments) |segment| {
+                        mask |= mapping[segment - 'a'];
                     }
                     const digit = self.mask_to_digit(mask);
-                    value = value * MAX_DIGIT + digit;
+                    value = value * NUM_DIGITS + digit;
                 }
+
                 self.total_sum += value;
                 continue;
             }
@@ -90,15 +142,16 @@ pub const Display = struct {
     }
 
     fn init_masks(self: *Display) void {
+        const mask = (1 << NUM_SEGMENTS) - 1;
         for (self.mask) |_, j| {
-            self.mask[j] = 0b1111111;
+            self.mask[j] = mask;
         }
     }
 
     fn build_mask_for_segments(_: Display, segments: []const u8) u8 {
         var mask: u8 = 0;
-        for (segments) |s| {
-            const shift = @intCast(u3, MAX_SEGMENT - 1 - (s - 'a'));
+        for (segments) |segment| {
+            const shift = @intCast(u3, NUM_SEGMENTS - 1 - (segment - 'a'));
             const bit = @as(u8, 1) << shift;
             mask |= bit;
         }
@@ -113,59 +166,22 @@ pub const Display = struct {
     fn restrict_masks(self: *Display, segments: []const u8) void {
         const possible = self.build_mask_for_segments(segments);
         const len = segments.len;
-        if (len == 2) {
-            self.restric_single_mask('c', possible);
-            self.restric_single_mask('f', possible);
-            return;
+        if (len >= 0 and len <= NUM_SEGMENTS) {
+            for (segments_for_len[len]) |segment| {
+                self.restric_single_mask(segment, possible);
+            }
         }
-        if (len == 3) {
-            self.restric_single_mask('a', possible);
-            self.restric_single_mask('c', possible);
-            self.restric_single_mask('f', possible);
-            return;
-        }
-        if (len == 4) {
-            self.restric_single_mask('b', possible);
-            self.restric_single_mask('c', possible);
-            self.restric_single_mask('d', possible);
-            self.restric_single_mask('f', possible);
-            return;
-        }
-        if (len == 5) {
-            self.restric_single_mask('a', possible);
-            self.restric_single_mask('d', possible);
-            self.restric_single_mask('g', possible);
-            return;
-        }
-        if (len == 6) {
-            self.restric_single_mask('a', possible);
-            self.restric_single_mask('b', possible);
-            self.restric_single_mask('f', possible);
-            self.restric_single_mask('g', possible);
-            return;
-        }
-        if (len == 7) {
-            self.restric_single_mask('a', possible);
-            self.restric_single_mask('b', possible);
-            self.restric_single_mask('c', possible);
-            self.restric_single_mask('d', possible);
-            self.restric_single_mask('e', possible);
-            self.restric_single_mask('f', possible);
-            self.restric_single_mask('g', possible);
-            return;
-        }
-        unreachable;
     }
 
     fn propagate_masks(self: *Display) void {
         while (true) {
             var changed: usize = 0;
             var j: usize = 0;
-            while (j < MAX_SEGMENT) : (j += 1) {
+            while (j < NUM_SEGMENTS) : (j += 1) {
                 const mj = self.mask[j];
                 if (@popCount(usize, mj) != 1) continue;
                 var k: usize = 0;
-                while (k < MAX_SEGMENT) : (k += 1) {
+                while (k < NUM_SEGMENTS) : (k += 1) {
                     if (k == j) continue;
                     const mk = self.mask[k];
                     if (@popCount(usize, mk) == 1) continue;
@@ -178,56 +194,21 @@ pub const Display = struct {
     }
 
     fn mask_to_digit(_: Display, mask: u8) u8 {
-        if (mask == 0b1110111) {
-            return 0;
-        }
-        if (mask == 0b1101111) {
-            return 6;
-        }
-        if (mask == 0b1111011) {
-            return 9;
-        }
-        if (mask == 0b0010010) {
-            return 1;
-        }
-        if (mask == 0b1011101) {
-            return 2;
-        }
-        if (mask == 0b1011011) {
-            return 3;
-        }
-        if (mask == 0b1101011) {
-            return 5;
-        }
-        if (mask == 0b0111010) {
-            return 4;
-        }
-        if (mask == 0b1010010) {
-            return 7;
-        }
-        if (mask == 0b1111111) {
-            return 8;
+        // TODO: this would be better with a hash, but the array only has 10
+        // entries, so we just do a linear search on it.
+        for (digit_mask) |m, p| {
+            if (mask == m) return @intCast(u8, p);
         }
         unreachable;
     }
 
     fn update_unique_counts(self: *Display, segments: []const u8) void {
         const len = segments.len;
-        if (len == 2) {
-            self.count_unique[1] += 1;
-            return;
-        }
-        if (len == 3) {
-            self.count_unique[7] += 1;
-            return;
-        }
-        if (len == 4) {
-            self.count_unique[4] += 1;
-            return;
-        }
-        if (len == 7) {
-            self.count_unique[8] += 1;
-            return;
+        if (len >= 0 and len <= NUM_SEGMENTS) {
+            const d = digits_for_len[len];
+            if (d > 0) {
+                self.count_unique[d] += 1;
+            }
         }
     }
 };
