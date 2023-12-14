4cee6ab7d17dd2014f9f1cc7c9b042202a4201a1
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2023-12-14 14:01:06 +0100

    2023 day 14: optimize
    I usually use a HashMap to represent a grid, because it is not always
    obvious how big the grid will have to be. But I decided to check how
    much of a performance impact that has, vs using a matrix.  It is quite
    significant: the time for part 2 went from 4s to 350ms (10x).

--- 2023/p14/island.zig
+++ 2023/p14/island.zig
@@ -4,9 +4,9 @@ const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
 pub const Platform = struct {
-    const CYCLES = 1000000000;
+    const NUM_CYCLES = 1000000000;
 
-    const Dir = enum {
+    const Direction = enum {
         N,
         W,
         S,
@@ -63,7 +63,7 @@ pub const Platform = struct {
         x: isize,
         y: isize,
 
-        pub fn init(dir: Dir) Delta {
+        pub fn init(dir: Direction) Delta {
             var self = Delta{ .x = 0, .y = 0 };
             switch (dir) {
                 .N => self.y = -1,
@@ -80,7 +80,7 @@ pub const Platform = struct {
         col: Iteration,
         delta: Delta,
 
-        pub fn init(dir: Dir, size: Pos) Tilt {
+        pub fn init(dir: Direction, size: Pair) Tilt {
             var self = Tilt{
                 .row = switch (dir) {
                     .N => Iteration.init(.Descending, size.y),
@@ -96,56 +96,56 @@ pub const Platform = struct {
         }
     };
 
-    const Cycle = struct {
-        const SIZE = std.meta.tags(Dir).len;
+    const State = struct {
+        const DIR_SIZE = std.meta.tags(Direction).len;
 
-        loads: [SIZE]usize,
+        loads: [DIR_SIZE]usize,
 
-        pub fn init() Cycle {
-            var self = Cycle{
-                .loads = [_]usize{0} ** SIZE,
+        pub fn init() State {
+            var self = State{
+                .loads = [_]usize{0} ** DIR_SIZE,
             };
             return self;
         }
 
-        pub fn setDirLoad(self: *Cycle, dir: Dir, load: usize) void {
+        pub fn setDirectionLoad(self: *State, dir: Direction, load: usize) void {
             self.loads[@intFromEnum(dir)] = load;
         }
     };
 
-    const Pos = struct {
+    const Pair = struct {
         x: usize,
         y: usize,
 
-        pub fn init(x: usize, y: usize) Pos {
-            var self = Pos{ .x = x, .y = y };
+        pub fn init(x: usize, y: usize) Pair {
+            var self = Pair{ .x = x, .y = y };
             return self;
         }
 
-        pub fn initFromSigned(x: isize, y: isize) Pos {
-            return Pos.init(@intCast(x), @intCast(y));
+        pub fn initFromSigned(x: isize, y: isize) Pair {
+            return Pair.init(@intCast(x), @intCast(y));
         }
     };
 
-    const Grid = std.AutoHashMap(Pos, Piece);
+    const GRID_MAX_SIZE = 120;
 
     allocator: Allocator,
     cycle: bool,
-    size: Pos,
-    data: Grid,
+    size: Pair,
+    data: [GRID_MAX_SIZE][GRID_MAX_SIZE]Piece,
 
     pub fn init(allocator: Allocator, cycle: bool) Platform {
         var self = Platform{
             .allocator = allocator,
             .cycle = cycle,
-            .size = Pos.init(0, 0),
-            .data = Grid.init(allocator),
+            .size = Pair.init(0, 0),
+            .data = undefined,
         };
         return self;
     }
 
     pub fn deinit(self: *Platform) void {
-        self.data.deinit();
+        _ = self;
     }
 
     pub fn addLine(self: *Platform, line: []const u8) !void {
@@ -153,9 +153,8 @@ pub const Platform = struct {
             self.size.x = line.len;
         }
         for (line, 0..) |c, x| {
-            const pos = Pos.init(x, self.size.y);
             const piece = Piece.parse(c);
-            _ = try self.data.getOrPutValue(pos, piece);
+            self.data[x][self.size.y] = piece;
         }
         self.size.y += 1;
     }
@@ -164,8 +163,7 @@ pub const Platform = struct {
         std.debug.print("Platform: {} x {}\n", .{ self.size.y, self.size.x });
         for (0..self.size.y) |y| {
             for (0..self.size.x) |x| {
-                const pos = Pos.init(x, y);
-                const piece = self.data.get(pos) orelse .Empty;
+                const piece = self.data[x][y];
                 std.debug.print("{c}", .{@intFromEnum(piece)});
             }
             std.debug.print("\n", .{});
@@ -178,9 +176,9 @@ pub const Platform = struct {
             return self.getLoad();
         }
 
-        var cycles = std.ArrayList(Cycle).init(self.allocator);
-        defer cycles.deinit();
-        var seen = std.AutoHashMap(Cycle, usize).init(self.allocator);
+        var states = std.ArrayList(State).init(self.allocator);
+        defer states.deinit();
+        var seen = std.AutoHashMap(State, usize).init(self.allocator);
         defer seen.deinit();
 
         var first_repeat: usize = 0;
@@ -188,8 +186,8 @@ pub const Platform = struct {
         var cycle_length: usize = 0;
         var step: usize = 0;
         while (cycle_length == 0) : (step += 1) {
-            const cycle = try self.runCycle();
-            const entry = try seen.getOrPut(cycle);
+            const state = try self.runCycle();
+            const entry = try seen.getOrPut(state);
             if (entry.found_existing) {
                 if (first_repeat == 0) {
                     first_repeat = step;
@@ -199,12 +197,12 @@ pub const Platform = struct {
                 }
             } else {
                 entry.value_ptr.* = step;
-                try cycles.append(cycle);
+                try states.append(state);
             }
         }
-        // step is zero-based, must decrease CYCLES by 1
-        const pos = (CYCLES - 1 - first_repeat) % cycle_length + first_repeat - cycle_length;
-        const wanted = cycles.items[pos];
+        // step is zero-based, must decrease NUM_CYCLES by 1
+        const pos = (NUM_CYCLES - 1 - first_repeat) % cycle_length + first_repeat - cycle_length;
+        const wanted = states.items[pos];
         return wanted.loads[3];
     }
 
@@ -213,8 +211,7 @@ pub const Platform = struct {
         for (0..self.size.y) |y| {
             var count: usize = 0;
             for (0..self.size.x) |x| {
-                const pos = Pos.init(x, y);
-                const piece = self.data.get(pos) orelse .Empty;
+                const piece = self.data[x][y];
                 if (piece != .Round) continue;
                 count += 1;
             }
@@ -223,52 +220,48 @@ pub const Platform = struct {
         return sum;
     }
 
-    fn runTilt(self: *Platform, dir: Dir) !void {
+    fn runTilt(self: *Platform, dir: Direction) !void {
         const tilt = Tilt.init(dir, self.size);
         var row = tilt.row.start;
         while (true) : (row += tilt.row.delta) {
             var col = tilt.col.start;
             while (true) : (col += tilt.col.delta) {
-                const src_pos = Pos.initFromSigned(col, row);
-                var src_entry = self.data.getEntry(src_pos);
-                const src_piece = src_entry.?.value_ptr.*;
+                const src_pos = Pair.initFromSigned(col, row);
+                const src_piece = self.data[src_pos.x][src_pos.y];
                 if (src_piece != .Round) {
                     if (tilt.col.done(col)) break;
                     continue;
                 }
-                var tgt_entry: ?Grid.Entry = null;
+                var tgt_pos_opt: ?Pair = null;
                 var step: isize = 1;
                 while (true) : (step += 1) {
                     var nx = col + tilt.delta.x * step;
                     if (nx < 0 or nx > self.size.x - 1) break;
                     var ny = row + tilt.delta.y * step;
                     if (ny < 0 or ny > self.size.y - 1) break;
-                    const new_pos = Pos.initFromSigned(nx, ny);
-                    const new_entry = self.data.getEntry(new_pos);
-                    const new_piece = new_entry.?.value_ptr.*;
+                    const new_pos = Pair.initFromSigned(nx, ny);
+                    const new_piece = self.data[new_pos.x][new_pos.y];
                     if (new_piece != .Empty) break;
-                    tgt_entry = new_entry;
+                    tgt_pos_opt = new_pos;
                 }
-                if (tgt_entry == null) {
-                    if (tilt.col.done(col)) break;
-                    continue;
+                if (tgt_pos_opt) |tgt_pos| {
+                    self.data[src_pos.x][src_pos.y] = .Empty;
+                    self.data[tgt_pos.x][tgt_pos.y] = .Round;
                 }
-                src_entry.?.value_ptr.* = .Empty;
-                tgt_entry.?.value_ptr.* = .Round;
                 if (tilt.col.done(col)) break;
             }
             if (tilt.row.done(row)) break;
         }
     }
 
-    fn runCycle(self: *Platform) !Cycle {
-        var cycle = Cycle.init();
-        for (std.meta.tags(Dir)) |dir| {
+    fn runCycle(self: *Platform) !State {
+        var state = State.init();
+        for (std.meta.tags(Direction)) |dir| {
             try self.runTilt(dir);
             const load = self.getLoad();
-            cycle.setDirLoad(dir, load);
+            state.setDirectionLoad(dir, load);
         }
-        return cycle;
+        return state;
     }
 };
 
